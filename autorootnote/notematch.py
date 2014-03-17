37add1d3080c2b40a92250f0c239851ecd75e896
unknown <mwicat@.(none)>
2014-03-17 11:04:49 +0100

    B is also a music note ;)

--- autorootnote/notematch.py
+++ autorootnote/notematch.py
@@ -1,6 +1,6 @@
 import re
 
-NOTE_RE = re.compile(r'.*[^a-zA-Z0-9]([CDEFGA]#?[0-9])[^a-zA-Z0-9].*', re.I)
+NOTE_RE = re.compile(r'.*[^a-zA-Z0-9]([BCDEFGA][#]?[0-9])[^a-zA-Z0-9].*', re.I)
 
 def extract_root_name(name):
     res = NOTE_RE.search(name)
@@ -8,7 +8,9 @@ def extract_root_name(name):
 
 
 if __name__ == '__main__':
-    test_names = ['K04Organ-D#1.wav', './synth/ARP Odyssey/Arp E Multi/Arp E C4.wav',
+    test_names = ['K04Organ-D#1.wav',
+                  'K04Organ-B1.wav',
+                  './synth/ARP Odyssey/Arp E Multi/Arp E C4.wav',
                   'BassK02-C4.wav', './floorfilling/Drum_Hits/Nu-Disco_PERC40.wav', 'HK002_d4.wav']
     for name in test_names:
         print name, extract_root_name(name)
